0f48557c374745a08da74bf695c6d7c6a2393e0b
Adarsh <adeee621@gmail.com>
2022-07-11 14:07:59 -0600

    Feature: Implemented Endpoint for Fetch Repos with repo.fork === false

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,36 @@
+import axios from 'axios';
 import { Router, Request, Response } from 'express';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 export const repos = Router();
+
+type Repository = typeof import('../../data/repos.json')[number];
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
-  res.status(200);
+  let reposFromGithub: Repository[] = [];
+  try {
+    // Fetch Repos from API
+    const resp = await axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
 
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+    reposFromGithub = resp.data;
+    // Fetch Repos from file location and convert it to JSobject
+    const data: Repository[] = JSON.parse(
+      readFileSync(
+        resolve(__dirname, '..', '..', 'data', 'repos.json')
+      ).toString()
+    );
+
+    // Combine Both Sources and Perform filter based form === false
+    const combined = [...reposFromGithub, ...data].filter(
+      (item) => item.fork === false
+    );
+    res.json(combined);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
